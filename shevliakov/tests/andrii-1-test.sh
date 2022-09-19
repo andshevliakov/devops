fcbc5d3eee507e2f97409aff0a4221eb135e66bb
ashevlyakov <shevlyakov.tlkpi@gmail.com>
2022-09-19 23:29:21 +0300

    task1 done

---
+++ shevliakov/tests/andrii-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m shevliakov -p andrii [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "andrii"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
